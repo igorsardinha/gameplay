f4a81bbbdb5905d87d57146688a57d797567ae78
Igor Sardinha <igor.sardinha@outlook.com>
2021-10-26 22:25:17 -0300

    Aula2 Commit

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,25 +1,22 @@
 import React, { useState } from 'react';
-import {
-    View,
-    Text,
-    Image,
-    StatusBar
-} from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import { View, Text, Image } from 'react-native';
 
 import { styles } from './styles';
 import IllustrationImg from '../../assets/illustration.png'
 import { ButtonIcon } from '../../components/ButtonIcon';
 
+
+
 export function SignIn() {
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
 
     return (
         <View style={styles.container}>
-            <StatusBar
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
-
             <Image
                 source={IllustrationImg}
                 style={styles.image}
@@ -27,19 +24,19 @@ export function SignIn() {
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize{`\n`}
-                    suas jogatinas{`\n`}
-                    facilmente
+                    Conecte-se {'\n'}
+                    e organize suas{'\n'}
+                    jogatinas
                 </Text>
 
                 <Text style={styles.subtitle}>
-                    Crie grupos para jogar seus games {`\n`}
+                    Crie grupos para jogar seus games {'\n'}
                     favoritos com seus amigos
                 </Text>
 
                 <ButtonIcon
                     title={'Entrar com discord'}
-                    activeOpacity={0.7}
+                    onPress={handleSignIn}
                 />
 
             </View>
